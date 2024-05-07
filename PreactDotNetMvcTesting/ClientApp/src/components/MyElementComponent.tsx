c44d9274dc79dde408a7a3f98940f2c6db05d4ee
RJ <richard.nader@fynydd.com>
2024-05-07 12:30:52 -0400

    Added web component registration for preact components. Fixes HMR for web components.

---
+++ PreactDotNetMvcTesting/ClientApp/src/components/MyElementComponent.tsx
@@ -0,0 +1,11 @@
+﻿
+
+export function MyElementComponent() {
+    return (
+        <div>
+            <h1>My Element</h1>
+            <p>This is a web component that can be developed as a Preact component.</p>
+            <p>I have yet to find a downside to using web components. They now support HMR.</p>
+        </div>
+    );
+}
--- PreactDotNetMvcTesting/ClientApp/src/main.ts
+++ PreactDotNetMvcTesting/ClientApp/src/main.ts
@@ -1,7 +1,10 @@
 import {h, render} from "preact";
+import register from "preact-custom-element";
+export { register };
 
 export { App } from "./components/App";
 export { MyTest } from "./components/MyTest";
+export { MyElementComponent } from './components/MyElementComponent';
 
 export function myRender(component: any, container: HTMLElement, props: any) {
     render(h(component, props), container);
